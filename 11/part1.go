294fad4bf26964b49f89f2d7339d7d7ab443c3f9
Stephen Englert <senglert@Stephens-MacBook-Pro-2.local>
2019-12-13 13:48:04 -0500

    finish part1 day 11

--- 11/part1.go
+++ 11/part1.go
@@ -10,6 +10,102 @@ import (
 	"strconv"
 )
 
+type Coordinate struct {
+	X int
+	Y int
+	Color int
+}
+
+func (coordinate *Coordinate) String() string {
+	return fmt.Sprintf("%d,%d", coordinate.X, coordinate.Y)
+}
+
+type Robot struct {
+	At Coordinate
+	Painted map[string]Coordinate
+	Direction string
+}
+
+func NewRobot() *Robot {
+	robot := &Robot{}
+	robot.Painted = make(map[string]Coordinate)
+	coordinate := Coordinate{0,0,0}
+	robot.At = coordinate
+	robot.Direction = "N"
+	return robot
+}
+
+func (robot *Robot) GetCurrentPanelColor() int {
+	if val, ok := robot.Painted[robot.At.String()]; ok {
+		return val.Color
+	} else {
+		return 0 // Assume panel is black if not in path
+	}
+}
+
+func (robot *Robot) PaintPanel(color int) {
+	robot.At.Color = color
+	robot.Painted[robot.At.String()] = robot.At
+}
+
+func (robot *Robot) Move(rotation int) {
+
+	currentX := robot.At.X
+	currentY := robot.At.Y
+
+	switch robot.Direction {
+	case "N":
+		if rotation == 0 {
+			robot.MoveToCoordinate(Coordinate{currentX, currentY - 1, 0})
+			robot.Direction = "W"
+		}
+
+		if rotation == 1 {
+			robot.MoveToCoordinate(Coordinate{currentX, currentY + 1, 0})
+			robot.Direction = "E"
+		}
+	case "S":
+		if rotation == 0 {
+			robot.MoveToCoordinate(Coordinate{currentX, currentY + 1, 0})
+			robot.Direction = "E"
+		}
+
+		if rotation == 1 {
+			robot.MoveToCoordinate(Coordinate{currentX, currentY - 1, 0})
+			robot.Direction = "W"
+		}
+	case "E":
+		if rotation == 0 {
+			robot.MoveToCoordinate(Coordinate{currentX + 1, currentY, 0})
+			robot.Direction = "N"
+		}
+
+		if rotation == 1 {
+			robot.MoveToCoordinate(Coordinate{currentX - 1, currentY, 0})
+			robot.Direction = "S"
+		}
+	case "W":
+		if rotation == 0 {
+			robot.MoveToCoordinate(Coordinate{currentX - 1, currentY, 0})
+			robot.Direction = "S"
+		}
+
+		if rotation == 1 {
+			robot.MoveToCoordinate(Coordinate{currentX + 1, currentY, 0})
+			robot.Direction = "N"
+		}
+	}
+}
+
+func (robot *Robot) MoveToCoordinate(coordinate Coordinate) {
+	if val, ok := robot.Painted[coordinate.String()]; ok {
+		robot.At = val
+	} else {
+		robot.At = coordinate
+	}
+}
+
+
 func main() {
 
 	// Get Data
@@ -30,41 +126,46 @@ func main() {
 		intCodes = append(intCodes, intCodeInt)
 	}
 
+	robot := paintRobot(intCodes)
+	fmt.Println(len(robot.Painted))
+
+}
+
+func paintRobot(intCodes []int) (*Robot) {
+
+	robot := NewRobot()
 	input := make(chan int)
 	output := make(chan int)
-	halted := make(chan bool)
-
-	go func() {
-		input <- 0
-	}()
+	needsInput := make(chan bool)
+	done := make(chan bool)
 
 
-	go processIntCodes(intCodes, input, output, halted)
+	go processIntCodes(intCodes, input, output, needsInput, done)
 
-	paintOrMove := 0;
+	paintOrMove := 0
 
-readChannel:
 	for {
 		select {
 		case out := <-output:
 			if paintOrMove == 0 {
-				fmt.Println("paint", out)
+				robot.PaintPanel(out)
+				//fmt.Println("paint", out)
 			}
 
 			if paintOrMove == 1 {
-				fmt.Println("move", out)
-				input <- 0
+				robot.Move(out)
+				//fmt.Println("move", out)
 			}
-
 			paintOrMove = 1 - paintOrMove
-		case <-halted:
-			fmt.Println("here")
-			break readChannel
+		case <-needsInput:
+			input <- robot.GetCurrentPanelColor()
+		case <- done:
+			return robot
 		}
 	}
 }
 
-func processIntCodes(originalIntCodes []int, input chan int, output chan int, halted chan bool) []int {
+func processIntCodes(originalIntCodes []int, input chan int, output chan int, needsInput chan bool, done chan bool) []int {
 
 	intCodes := make([]int, len(originalIntCodes))
 	copy(intCodes, originalIntCodes)
@@ -85,15 +186,13 @@ func processIntCodes(originalIntCodes []int, input chan int, output chan int, ha
 
 	halt := false
 	for halt == false {
-
 		movePointer := true
 		instruction := fmt.Sprintf("%05d", intCodes[at])
 
 		opCode := instruction[3:]
 
 		if opCode == "99" {
-			<-input
-			halted <- true
+			done <- true
 			break
 		}
 
@@ -150,13 +249,13 @@ func processIntCodes(originalIntCodes []int, input chan int, output chan int, ha
 				intCodes = (growMemory(intCodes, param1 + 1))
 			}
 
+			needsInput <- true
 			intCodes[param1] = <-input
 		}
 
 		if opCode == "04" {
 			param1 := intCodes[at + 1]
 			param1Mode := instruction[2:3]
-			//fmt.Println(getValue(intCodes, param1, param1Mode, relativeBase))
 			output <- getValue(intCodes, param1, param1Mode, relativeBase)
 		}
 
@@ -241,7 +340,6 @@ func processIntCodes(originalIntCodes []int, input chan int, output chan int, ha
 		if movePointer {
 			at += step[opCode]
 		}
-
 	}
 
 	return intCodes
